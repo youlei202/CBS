4427cce347b16e7a628f426c730dcfcdce67da05
youlei202 <lei.you@pm.me>
2023-04-25 17:00:22 +0200

    use fewer samples for fisher matrix

--- scripts/sweep_mnist_mlpnet_optimal_transport.sh
+++ scripts/sweep_mnist_mlpnet_optimal_transport.sh
@@ -30,8 +30,8 @@ DATA_PATH=../datasets/
 CONFIG_PATH=./configs/mlpnet_mnist_config_optimal_transport.yaml
 PRUNER=optimal_transport
 EPOCHS=100
-FISHER_SUBSAMPLE_SIZE=10000
-FISHER_MINI_BSZ=1
+FISHER_SUBSAMPLE_SIZE=240
+FISHER_MINI_BSZ=100
 LOAD_FROM="./checkpoints/mnist_25_epoch_93.97.ckpt"
 BSZ=64
 
